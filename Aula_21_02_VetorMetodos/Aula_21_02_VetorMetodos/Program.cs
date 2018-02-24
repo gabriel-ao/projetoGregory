df3057822503c2f6cf84322c19c0b7afdd414a42
Gabriel Almeida de Oliveira <gabriel-ao@hotmail.com>
2018-02-24 20:32:42 -0300

    tentando resolver o erro do ref
    codigo sem erro, porem os dados não estão passando, buscar informar com o professor sobre o erro

--- Aula_21_02_VetorMetodos/Aula_21_02_VetorMetodos/Program.cs
+++ Aula_21_02_VetorMetodos/Aula_21_02_VetorMetodos/Program.cs
@@ -4,45 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aula_21_02_VetorMetodos
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Aula_21_02_VetorMetodos {
+    class Program {
+
+        static void Main(string[] args) {
             menu();
             // fechando tests do git
         }
 
-        public static void menu()
-        {
-            
+        public static void menu() {
+            while (true) {
                 Console.Clear();
                 Console.WriteLine("Cadastre 50 alunos \n\n" +
                 "o que vc deseha fazer: \n\n" +
                 "1 - cadastrar aluno. \n\n" +
                 "2 - listar alunos cadastrados. \n\n" +
-                "3 - sair do programa. \n\n" +
+                "3 - cancelar aluno. \n\n" +
+                "4 - sair do programa \n\n" +
+                "=============================== \n\n"+
                 "Digite o numero desejado: ");
 
                 int opcao = Int32.Parse(Console.ReadLine());
-            while (opcao !=3)
-            { 
+
                 int tamanhoVetor = 2;
                 int[] codigoAluno = new int[tamanhoVetor];
                 string[] nome = new string[tamanhoVetor];
                 int[] idade = new int[tamanhoVetor];
                 char[] sexo = new char[tamanhoVetor];
 
-                switch (opcao)
-                {
+                int indiceProximoAluno = 0;
+
+
+                switch (opcao) {
                     case 1:
-                        cadastro(ref codigoAluno, ref nome, ref idade, ref sexo);
+                        cadastro(ref codigoAluno, ref nome, ref idade, ref sexo, ref indiceProximoAluno);
                         break;
                     case 2:
                         listar(ref codigoAluno, ref nome, ref idade, ref sexo);
                         break;
                     case 3:
+                        cancelar(ref codigoAluno, ref nome, ref idade, ref sexo);
+                        break;
+                    case 4:
                         fechar();
                         break;
                     default:
@@ -51,76 +54,85 @@ namespace Aula_21_02_VetorMetodos
                 }
                 Console.ReadKey();
             }
+
         }
 
 
-        public static void cadastro(ref int[] codigoAluno, ref string[] nome, ref int[] idade, ref char[] sexo)
-        {
+        private static void cancelar(ref int[] codigoAluno, ref string[] nome, ref int[] idade, ref char[] sexo) {
+
+            Console.WriteLine("digite o nome do aluno que de");
+
+
+
+        }
+
+
+        public static void cadastro(ref int[] codigoAluno, ref string[] nome, ref int[] idade, ref char[] sexo, ref int indiceProximoAluno) {
             int opcao;
 
-            for (int i = 0; i < idade.Length; i++)
-            {
-                Console.WriteLine("====================================");
-                Console.WriteLine("Digite o codigo do aluno");
-                codigoAluno[i]= Convert.ToInt32(Console.ReadLine());
+            for (; indiceProximoAluno < idade.Length; indiceProximoAluno++) {
+
+                //indice do aluno automatico
+                if(indiceProximoAluno == 0) {
+                    codigoAluno[indiceProximoAluno] = indiceProximoAluno++;
+                } else {
+                    codigoAluno[indiceProximoAluno] = indiceProximoAluno++;
+                }
+
+
                 Console.WriteLine("====================================");
                 Console.WriteLine("Digite o seu nome: ");
-                nome[i] = Console.ReadLine();
+                nome[indiceProximoAluno] = Console.ReadLine();
                 Console.WriteLine("====================================");
                 Console.WriteLine("Digite sua idade: ");
-                idade[i] = Convert.ToInt32(Console.ReadLine());
+                idade[indiceProximoAluno] = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("====================================");
                 Console.WriteLine("Digite o seu sexo: ");
-                sexo[i] = Convert.ToChar(Console.ReadLine());
+                sexo[indiceProximoAluno] = Convert.ToChar(Console.ReadLine());
                 Console.WriteLine("====================================");
 
-                 Console.WriteLine("\n\nDeseja cadastrar novamente (1-SIM / 2-NÃO)");
-                  opcao = Int32.Parse(Console.ReadLine());
-                  if (opcao == 1)
-                  {
-                      continue;
-                  }
-                  else if (opcao == 2)
-                  {
-                      break;
-                  }
-                  else
-                  {
-                      Console.WriteLine("opcao errada");
-                  }
+                Console.WriteLine("\n\nDeseja cadastrar novamente (1-SIM / 2-NÃO)");
+                opcao = Int32.Parse(Console.ReadLine());
+                if (opcao == 1) {
+                    continue;
+                } else if (opcao == 2) {
+                    break;
+                } else {
+                    Console.WriteLine("opcao errada");
+                }
             }
         }
 
-        public static void listar(ref int[] codigoAluno, ref string[] nome, ref int[] idade, ref char[] sexo)
-        {
+        public static void listar(ref int[] codigoAluno, ref string[] nome, ref int[] idade, ref char[] sexo) {
 
-            for (int i = 0; i < idade.Length; i++)
-            {
-                Console.WriteLine("Nome: {2}" + nome[i]);
-                Console.WriteLine("Idade: {3}" + idade[i]);
+            for (int i = 0; i < idade.Length; i++) {
+                //if (codigoAluno[i] != 0) {
+
+                    Console.WriteLine("{0}codigo  {1}nome {2}idade  {3}sexo", codigoAluno[i], nome[i], idade[i], sexo[i]);
+                    //Console.WriteLine("Codigo: " + codigoAluno[i]);
+                    //Console.WriteLine("Nome: " + nome[i]);
+                    //Console.WriteLine("Idade: " + idade[i]);
+                //}
             }
             Console.ReadKey();
             Console.Clear();
-            menu();
+
         }
 
 
 
-        public static void fechar()
-        {
+        public static void fechar() {
             // comando utilizado para fechar o programa direto
             Console.WriteLine("programa finalizado, até mais!!!!");
             Console.ReadKey();
             Environment.Exit(0);
         }
 
-        public static void aviso()
-        {
+        public static void aviso() {
             Console.Clear();
             Console.WriteLine("\n\n <!!--Esta opção não foi encontrada, favor digitar opçao existente !!! --!!>" +
                 "pressione ENTER para voltar ao menu");
             Console.ReadKey();
-            menu();
         }
     }
 }
